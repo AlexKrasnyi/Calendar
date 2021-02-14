ec317b6c73542eda839c8afb007baac362567816
mkrasnyi <mkrasnyi@playtika.com>
2021-02-14 19:02:49 +0200

    fixed rest issues with multi select

--- srs/js/calendar.js
+++ srs/js/calendar.js
@@ -1,23 +1,25 @@
-
-export function users(arr, user) {
-    for(let i = 0; i < arr.length; i++) {
-        user.insertAdjacentHTML('beforeend', `
-        <option value="${arr[i]}">${arr[i]}</option> 
+export function users (participantsDict, user) {
+  user.insertAdjacentHTML('beforeend', `
+        <option value="all users">all users</option> 
         `)
-    }
+  for (let i = 0; i < participantsDict.length; i++) {
+    user.insertAdjacentHTML('beforeend', `
+        <option value="${participantsDict[i].value}">${participantsDict[i].label}</option> 
+        `)
+  }
 }
 
-export function calendarHead(arr, data){
-    for(let i =0; i < arr.length; i++) {
-        data.insertAdjacentHTML('beforeEnd', `
+export function calendarHead (arr, data) {
+  for (let i = 0; i < arr.length; i++) {
+    data.insertAdjacentHTML('beforeEnd', `
         <th class="row__sell">${arr[i]}</th>
         `)
-    }
+  }
 }
 
-export function dayTimes(arr, data, days) {
-    for (let i = 0; i < arr.length; i++){
-        data.insertAdjacentHTML('beforeend', `
+export function dayTimes (arr, data, days) {
+  for (let i = 0; i < arr.length; i++) {
+    data.insertAdjacentHTML('beforeend', `
             <tr class="calendar__row row">
                 <td class="row__sell">${arr[i]}:00</td>
                 <td class="row__sell" id="${days[0].toLowerCase()}_${arr[i]}"></td>
@@ -27,68 +29,69 @@ export function dayTimes(arr, data, days) {
                 <td class="row__sell" id="${days[4].toLowerCase()}_${arr[i]}"></td>
             </tr>
         `)
-    }    
+  }
 }
 
-export function createEvent(dataEvent, user, data) {
-    dataEvent.forEach(el => {
-        const activeCell = document.getElementById(el);
-        displayEvent(activeCell, data, el)
-        modalWindow (el, activeCell, data);
-    })
+export function createEvent (dataEvent, user, data) {
+  dataEvent.forEach(el => {
+    const activeCell = document.getElementById(el)
+    displayEvent(activeCell, data, el)
+    modalWindow(el, activeCell, data)
+  })
 }
 
-export function userFilter(user, dataEvent, data) {
-    user.addEventListener('change', () => {
-        createEvent(dataEvent, user, data)
-        if(user.value !== 'all users') {
-            dataEvent.forEach(el => {
-                const userEv = data[el].eventPartisipans;
-                console.log(user.value, user.value.includes(userEv));
-                if(!userEv.includes(user.value)) {
-                    const delCell = document.getElementById(el);
-                    console.log('el', delCell );
-                    delCell.innerHTML = '';
-                    delCell.style.backgroundColor = '';
-                } 
-            })
+export function userFilter (user, dataEvent, data) {
+  user.addEventListener('change', () => {
+    createEvent(dataEvent, user, data)
+    if (user.value !== 'all users') {
+      dataEvent.forEach(el => {
+        const userEv = data[el].eventPartisipans
+        console.log(user.value, user.value.includes(userEv))
+        if (!userEv.includes(user.value)) {
+          const delCell = document.getElementById(el)
+          console.log('el', delCell)
+          delCell.innerHTML = ''
+          delCell.style.backgroundColor = ''
         }
-    })
+      })
+    }
+  })
 }
 
-function displayEvent(activeCell, data, el) {
-    activeCell.style.backgroundColor = '#D3F5B4';
-        activeCell.innerHTML = `<div class="activeEvent">
+function displayEvent (activeCell, data, el) {
+  activeCell.style.backgroundColor = '#D3F5B4'
+  activeCell.innerHTML = `<div class="activeEvent">
         <p class="activeEvent__name">${data[el].eventName}</p>
         <button type="button" class="activeEvent__button" id="button_${el}">&#10006</button>
         </div>`
 }
 
-function abolishmentDeleteEvent() {
-    const modal = document.getElementsByClassName('modal')[0];
-    const modalButtonCansel = document.getElementsByClassName('modal__button')[0];
-    modalButtonCansel.addEventListener('click', () => {
-         modal.remove();
-    });
+function abolishmentDeleteEvent () {
+  const modal = document.getElementsByClassName('modal')[0]
+  const modalButtonCansel = document.getElementsByClassName('modal__button')[0]
+  modalButtonCansel.addEventListener('click', () => {
+    modal.remove()
+  })
 }
 
-function removeEvent(cell, data, el) {
-    const modal = document.getElementsByClassName('modal')[0];
-    const modalButtonOk = document.getElementsByClassName('modal__button')[1];
-    modalButtonOk.addEventListener('click', () => {
-        cell.style.backgroundColor = '';
-        cell.innerHTML = '';
-        delete data[el];;
-        localStorage.setItem('calendarData', JSON.stringify(data))
-        modal.remove();
-    })
+function removeEvent (cell, data, el) {
+  const modal = document.getElementsByClassName('modal')[0]
+  const modalButtonOk = document.getElementsByClassName('modal__button')[1]
+  modalButtonOk.addEventListener('click', () => {
+    cell.style.backgroundColor = ''
+    cell.innerHTML = ''
+    delete data[el]
+
+    localStorage.setItem('calendarData', JSON.stringify(data))
+    modal.remove()
+  })
 }
 
 function modalWindow (el, activeCell, data) {
-    const activeEventButton = document.getElementById(`button_${el}`);
-        activeEventButton.addEventListener('click', () => {
-            const modalBacground = document.getElementsByClassName('header');
-            document.body.insertAdjacentHTML('afterbegin', `
+  const activeEventButton = document.getElementById(`button_${el}`)
+  activeEventButton.addEventListener('click', () => {
+    const modalBacground = document.getElementsByClassName('header')
+    document.body.insertAdjacentHTML('afterbegin', `
                  <div class="modal">
                      <div class="modal__window">
                          <p>Are yor sure you want to delete " ${data[el].eventName} " event?</p>
@@ -99,8 +102,8 @@ function modalWindow (el, activeCell, data) {
                      </div>
                  </div>
             `)
-        abolishmentDeleteEvent();
-        removeEvent(activeCell, data, el);
-    })
+    abolishmentDeleteEvent()
+    removeEvent(activeCell, data, el)
+  })
 }
 
--- srs/js/multiselect.js
+++ srs/js/multiselect.js
@@ -1,272 +1,249 @@
-const Motus = {};
-(function () {
-  const createMultiselect = function (element, data, selectCb, options) {
+const createMultiselect = function (element, data, selectCb, options) {
 
-    const labels = {}
+  const labels = {}
 
-    labels.emptyText = (options && options.emptyText) ? options.emptyText : 'Select an option'
-    labels.selectedText = (options && options.selectedText) ? options.selectedText : 'Selected'
-    labels.selectedAllText = (options && options.selectedAllText) ? options.selectedAllText : 'Select All'
-    labels.title = (options && options.title) ? options.title : ''
+  labels.emptyText = (options && options.emptyText) ? options.emptyText : 'Select an option'
+  labels.selectedText = (options && options.selectedText) ? options.selectedText : 'Selected'
+  labels.selectedAllText = (options && options.selectedAllText) ? options.selectedAllText : 'Select All'
+  labels.title = (options && options.title) ? options.title : ''
 
-    //define the elements
-    const container = document.createElement('div')
-    const multiselectLabel = document.createElement('div')
-    const dataContainer = document.createElement('div')
-    const button = document.createElement('button')
-    const searchField = document.createElement('input')
-    const clearSelection = document.createElement('span')
-    const carret = document.createElement('b')
-    const list = document.createElement('ul')
+  //define the elements
+  const container = document.createElement('div')
+  const multiselectLabel = document.createElement('div')
+  const dataContainer = document.createElement('div')
+  const button = document.createElement('button')
+  const searchField = document.createElement('input')
+  const clearSelection = document.createElement('span')
+  const carret = document.createElement('b')
+  const list = document.createElement('ul')
 
-    //set the ids
-    const elementId = element.id
-    container.setAttribute('id', 'multiselect_container_' + elementId)
-    dataContainer.setAttribute('id', 'multiselect_datacontainer_' + elementId)
-    multiselectLabel.setAttribute('id', 'multiselect_label_' + elementId)
-    button.setAttribute('id', 'multiselect_button_' + elementId)
-    list.setAttribute('id', 'multiselect_list_' + elementId)
+  //set the ids
+  const elementId = element.id
+  container.setAttribute('id', 'multiselect_container_' + elementId)
+  dataContainer.setAttribute('id', 'multiselect_datacontainer_' + elementId)
+  multiselectLabel.setAttribute('id', 'multiselect_label_' + elementId)
+  button.setAttribute('id', 'multiselect_button_' + elementId)
+  list.setAttribute('id', 'multiselect_list_' + elementId)
 
-    const _fnIsChild = function (element, parent) {
-      let node = element.parentNode
-      while (node) {
-        if (node === parent) {
-          return true
-        }
-        node = node.parentNode
+  const _fnIsChild = function (element, parent) {
+    let node = element.parentNode
+    while (node) {
+      if (node === parent) {
+        return true
       }
-      return false
+      node = node.parentNode
     }
+    return false
+  }
 
-    const _selectionText = function (element) {
-      let text = ''
-      const selection = element.querySelectorAll('input:checked')
-      if (selection.length === 0) {
-        text = labels.emptyText
-      } else if (selection.length > 3) {
-        text = selection.length + ' ' + labels.selectedText
-      } else {
-        const arr = []
-        for (let i = 0; i < selection.length; i++) {
-          arr.push(selection[i].parentNode.textContent)
-        }
-        text = arr.join(',')
+  const _selectionText = function (element) {
+    let text = ''
+    const selection = element.querySelectorAll('input:checked')
+    if (selection.length === 0) {
+      text = labels.emptyText
+    } else if (selection.length > 3) {
+      text = selection.length + ' ' + labels.selectedText
+    } else {
+      const arr = []
+      for (let i = 0; i < selection.length; i++) {
+        arr.push(selection[i].parentNode.textContent)
       }
-      return text
+      text = arr.join(',')
     }
+    return text
+  }
 
-    const _openList = function (e) {
-      list.style.display = 'block'
-      e.srcElement.children[0].focus()
-    }
+  const _openList = function (e) {
+    list.style.display = 'block'
+    e.srcElement.children[0].focus()
+  }
 
-    const _selectItem = function (e) {
-      let text = _selectionText(container)
-      container.getElementsByTagName('button')[0]
-        .children[0].setAttribute('placeholder', text)
+  const _selectItem = function (e) {
+    let text = _selectionText(container)
+    container.getElementsByTagName('button')[0]
+      .children[0].setAttribute('placeholder', text)
 
-      if (selectCb) {
-        const selectionElements = container.querySelectorAll('input:checked')
-        const selection = []
-        for (let i = 0; i < selectionElements.length; i++) {
-          selection.push(selectionElements[i].value)
-        }
-        selectCb(selection)
+    if (selectCb) {
+      const selectionElements = container.querySelectorAll('input:checked')
+      const selection = []
+      for (let i = 0; i < selectionElements.length; i++) {
+        selection.push(selectionElements[i].value)
       }
-
+      selectCb(selection)
     }
 
-    const _clearSearch = function () {
+  }
+
+  const _clearSearch = function () {
+    const elements = container.getElementsByTagName('li')
+    for (let i = 0; i < elements.length; i++) {
+      elements[i].style.display = ''
+    }
+  }
+
+  const _performSearch = function (e) {
+    if (e.which !== 13 && e.which !== 38 && e.which !== 40) {
+      let active = list.getElementsByClassName('multiselect-label--active')
+      if (active.length > 0) {
+        active[0].classList.remove('multiselect-label--active')
+      }
+      let first = true
+      let filter = e.srcElement.value.toUpperCase()
       const elements = container.getElementsByTagName('li')
       for (let i = 0; i < elements.length; i++) {
-        elements[i].style.display = ''
-      }
-    }
-
-    const _performSearch = function (e) {
-      if (e.which !== 13 && e.which !== 38 && e.which !== 40) {
-        let active = list.getElementsByClassName('multiselect-label--active')
-        if (active.length > 0) {
-          active[0].classList.remove('multiselect-label--active')
-        }
-        let first = true
-        let filter = e.srcElement.value.toUpperCase()
-        const elements = container.getElementsByTagName('li')
-        for (let i = 0; i < elements.length; i++) {
-          const cb = elements[i].getElementsByTagName('label')[0].textContent
-          if (cb.toUpperCase().indexOf(filter) !== -1) {
-            if (first) {
-              first = false
-              elements[i].children[0].children[0].classList.add('multiselect-label--active')
-            }
-            elements[i].style.display = ''
-          } else {
-            elements[i].style.display = 'none'
+        const cb = elements[i].getElementsByTagName('label')[0].textContent
+        if (cb.toUpperCase().indexOf(filter) !== -1) {
+          if (first) {
+            first = false
+            elements[i].children[0].children[0].classList.add('multiselect-label--active')
           }
+          elements[i].style.display = ''
+        } else {
+          elements[i].style.display = 'none'
         }
       }
     }
+  }
 
-    const _fnClearSelection = function (e) {
-      const inputs = list.getElementsByTagName('input')
-      for (var i = 0; i < inputs.length; i++) {
-        if (inputs[i].checked) {
-          inputs[i].parentNode.click()
-        }
+  const _fnClearSelection = function (e) {
+    const inputs = list.getElementsByTagName('input')
+    for (var i = 0; i < inputs.length; i++) {
+      if (inputs[i].checked) {
+        inputs[i].parentNode.click()
       }
-      e.stopPropagation()
     }
+    e.stopPropagation()
+  }
 
-    const _fnSelectAll = function (e) {
-      const inputs = list.getElementsByTagName('input')
-      for (let i = 0; i < inputs.length; i++) {
-        if (!inputs[i].checked) {
-          inputs[i].parentNode.click()
-        }
+  const _fnSelectAll = function (e) {
+    const inputs = list.getElementsByTagName('input')
+    for (let i = 0; i < inputs.length; i++) {
+      if (!inputs[i].checked) {
+        inputs[i].parentNode.click()
       }
-      e.stopPropagation()
     }
+    e.stopPropagation()
+  }
 
-    container.classList.add('multiselect-container')
-    multiselectLabel.classList.add('multiselect-label')
-    multiselectLabel.innerHTML = labels.title
-    dataContainer.classList.add('multiselect-data-container')
-    button.classList.add('multiselect-button')
+  container.classList.add('multiselect-container')
+  multiselectLabel.classList.add('multiselect-label')
+  multiselectLabel.innerHTML = labels.title
+  dataContainer.classList.add('multiselect-data-container')
+  button.classList.add('multiselect-button')
 
-    searchField.setAttribute('type', 'text')
-    searchField.setAttribute('placeholder', labels.emptyText)
-    searchField.classList.add('multiselect-text')
-    searchField.addEventListener('keyup', _performSearch)
+  searchField.setAttribute('type', 'text')
+  searchField.setAttribute('placeholder', labels.emptyText)
+  searchField.classList.add('multiselect-text')
+  searchField.addEventListener('keyup', _performSearch)
 
-    clearSelection.classList.add('multiselect-clear')
-    clearSelection.innerHTML = 'X'
-    clearSelection.addEventListener('click', _fnClearSelection)
+  clearSelection.classList.add('multiselect-clear')
+  clearSelection.innerHTML = 'X'
+  clearSelection.addEventListener('click', _fnClearSelection)
 
-    carret.classList.add('carret')
+  carret.classList.add('carret')
 
-    button.appendChild(searchField)
-    button.appendChild(clearSelection)
-    button.appendChild(carret)
+  button.appendChild(searchField)
+  button.appendChild(clearSelection)
+  button.appendChild(carret)
 
-    button.addEventListener('click', _openList)
+  button.addEventListener('click', _openList)
 
-    list.classList.add('multiselect-list')
+  list.classList.add('multiselect-list')
 
-    for (var i = -1; i < data.length; i++) {
-      const item = document.createElement('li')
-      const a = document.createElement('a')
-      const label = document.createElement('label')
-      const input = document.createElement('input')
+  for (var i = -1; i < data.length; i++) {
+    const item = document.createElement('li')
+    const a = document.createElement('a')
+    const label = document.createElement('label')
+    const input = document.createElement('input')
 
-      a.setAttribute('tabindex', '0')
+    a.setAttribute('tabindex', '0')
 
-      label.classList.add('multiselect-item-label')
+    label.classList.add('multiselect-item-label')
 
-      if (i === -1) {
-        a.addEventListener('click', _fnSelectAll)
-        label.appendChild(document.createTextNode('Select all'))
-        label.classList.add('multiselect-item-label--select-all')
-      } else {
-        if (i === 0) {
-          label.classList.add('multiselect-item-label--active')
-        }
-        input.setAttribute('type', 'checkbox')
-        input.setAttribute('class', 'multiselect-checkbox')
-        label.appendChild(input)
-        input.setAttribute('value', data[i].value)
-        input.addEventListener('change', _selectItem)
-        label.appendChild(document.createTextNode(data[i].label))
+    if (i === -1) {
+      a.addEventListener('click', _fnSelectAll)
+      label.appendChild(document.createTextNode('Select all'))
+      label.classList.add('multiselect-item-label--select-all')
+    } else {
+      if (i === 0) {
+        label.classList.add('multiselect-item-label--active')
       }
-      a.appendChild(label)
-      item.appendChild(a)
-      list.appendChild(item)
+      input.setAttribute('type', 'checkbox')
+      input.setAttribute('class', 'multiselect-checkbox')
+      label.appendChild(input)
+      input.setAttribute('value', data[i].value)
+      input.addEventListener('change', _selectItem)
+      label.appendChild(document.createTextNode(data[i].label))
     }
+    a.appendChild(label)
+    item.appendChild(a)
+    list.appendChild(item)
+  }
 
-    dataContainer.appendChild(button)
-    dataContainer.appendChild(list)
-    container.appendChild(multiselectLabel)
-    container.appendChild(dataContainer)
-    element.appendChild(container)
+  dataContainer.appendChild(button)
+  dataContainer.appendChild(list)
+  container.appendChild(multiselectLabel)
+  container.appendChild(dataContainer)
+  element.appendChild(container)
 
-    //Change to the specific window
-    document.addEventListener('click', function (e) {
-      if (!_fnIsChild(e.target, container)) {
-        list.style.display = 'none'
-        searchField.value = ''
-        _clearSearch()
-      }
-    })
+  //Change to the specific window
+  document.addEventListener('click', function (e) {
+    if (!_fnIsChild(e.target, container)) {
+      list.style.display = 'none'
+      searchField.value = ''
+      _clearSearch()
+    }
+  })
 
-    document.addEventListener('keyup', function (e) {
-      if (list.style.display === 'block') {
-        //mouse down
-        if (e.which === 40) {
-          let active = list.getElementsByClassName(
-            'multiselect-label--active'
-          )[0]
-          let next = active.parentNode.parentNode.nextSibling
-          //Find the next visible element
-          while (next && next.style && next.style.display === 'none') {
-            next = next.nextSibling
-          }
-          if (next) {
-            active.classList.remove('multiselect-label--active')
-            next
-              .getElementsByClassName('multiselect-label')[0]
-              .classList.add('multiselect-label--active')
-            next.children[0].focus()
-            searchField.focus()
-            e.preventDefault()
-          }
-        } else if (e.which === 38) {
-          //mouse up
-          const active = list.getElementsByClassName(
-            'multiselect-label--active'
-          )[0]
-          let prev = active.parentNode.parentNode.previousSibling
-          //Find the previous visible element
-          while (prev && prev.style && prev.style.display === 'none') {
-            prev = prev.previousSibling
-          }
-          if (prev) {
-            active.classList.remove('multiselect-label--active')
-            prev
-              .getElementsByClassName('multiselect-label')[0]
-              .classList.add('multiselect-label--active')
-            prev.children[0].focus()
-            searchField.focus()
-            e.preventDefault()
-          }
-        } else if (e.which === 13) {
-          // enter
-          list.getElementsByClassName('multiselect-label--active')[0].click()
+  document.addEventListener('keyup', function (e) {
+    if (list.style.display === 'block') {
+      //mouse down
+      if (e.which === 40) {
+        let active = list.getElementsByClassName(
+          'multiselect-label--active'
+        )[0]
+        let next = active.parentNode.parentNode.nextSibling
+        //Find the next visible element
+        while (next && next.style && next.style.display === 'none') {
+          next = next.nextSibling
+        }
+        if (next) {
+          active.classList.remove('multiselect-label--active')
+          next
+            .getElementsByClassName('multiselect-label')[0]
+            .classList.add('multiselect-label--active')
+          next.children[0].focus()
+          searchField.focus()
           e.preventDefault()
         }
+      } else if (e.which === 38) {
+        //mouse up
+        const active = list.getElementsByClassName(
+          'multiselect-label--active'
+        )[0]
+        let prev = active.parentNode.parentNode.previousSibling
+        //Find the previous visible element
+        while (prev && prev.style && prev.style.display === 'none') {
+          prev = prev.previousSibling
+        }
+        if (prev) {
+          active.classList.remove('multiselect-label--active')
+          prev
+            .getElementsByClassName('multiselect-label')[0]
+            .classList.add('multiselect-label--active')
+          prev.children[0].focus()
+          searchField.focus()
+          e.preventDefault()
+        }
+      } else if (e.which === 13) {
+        // enter
+        list.getElementsByClassName('multiselect-label--active')[0].click()
+        e.preventDefault()
       }
-    })
-  }
-  const exportObj = {
-    init: function (element, data, selectCb, options) {
-      createMultiselect(element, data, selectCb, options)
     }
-  }
-
-  Motus.ElementMultiselect = exportObj
-
-})()
-
-const data = [
-  { label: 'Volodymyr Vasiliev', value: 'VVasiliev' },
-  { label: 'Ivan Ivanov', value: 'IIvanov' },
-  { label: 'Dmytro Duritskiy', value: 'DDuritskiy3' },
-  { label: 'Ihor Marchuk', value: 'IMarchuk' },
-  { label: 'Maksym Vorobiev', value: 'MVorobiev' },
-  { label: 'Oleksandr Vakulenko', value: 'OVakulenko' }
-]
-const element = document.getElementById('event__users2')
-
-const select = function (data) {
-  console.log(data)
+  })
 }
 
-Motus.ElementMultiselect.init(element, data, select)
+export function initMultiSelect (element, data, selectCb, options) {
+       createMultiselect(element, data, selectCb, options)
+}
